5e28694b0ac74119accecb8695929cd2621cd382
Colin TENAGUILLO <colin.tenaguillo@ynov.com>
2019-09-07 15:02:27 +0200

    modif image character

--- src/components/gameplay/Character/Character.js
+++ src/components/gameplay/Character/Character.js
@@ -1,8 +1,29 @@
 import React, { Component } from "react";
+import Inventaire from "./list-inventaire";
 
 class Character extends Component {
-    constructor() {
-        super()
+    // constructor() {
+    //     super()
+    // }
+
+    handleClickHelmet = () => {
+        const div = document.getElementById("list-inventaire")
+        div.appendChild(<Inventaire />)
+    }
+    handleClickChest = () => {
+
+    }
+    handleClickMainHand = () => {
+
+    }
+    handleClickLegs = () => {
+
+    }
+    handleClickBoots = () => {
+
+    }
+    handleClickOffHand = () => {
+
     }
 
     render() {
@@ -10,18 +31,19 @@ class Character extends Component {
             <div id="Character">
                 <h3 className="text-center">Character</h3>
                 <div class="character">
-                    <div className="list-objet">
-                        <div className="objet"></div>
-                        <div className="objet"></div>
-                        <div className="objet"></div>
+                    <div className="list-equip">
+                        <div onClick={this.handleClickHelmet} className="objet helmet"></div>
+                        <div onClick={this.handleClickChest} className="objet chest"></div>
+                        <div onClick={this.handleClickMainHand} className="objet mainhand"></div>
                     </div>
-                    <div class="clear"></div>
                     <div class="element-character"></div>
-                    <div className="list-objet">
-                        <div className="objet"></div>
-                        <div className="objet"></div>
-                        <div className="objet"></div>
+                    <div className="list-equip">
+                        <div onClick={this.handleClickLegs} className="objet legs"></div>
+                        <div onClick={this.handleClickBoots} className="objet boots"></div>
+                        <div onClick={this.handleClickOffHand} className="objet offhand"></div>
                     </div>
+                </div>
+                <div id="list-inventaire">
                 </div>
             </div>
         )
