3cb671e17b6aa4fd121453fce96cfb4ce7f0c66f
XIMENES <clement.ximenes@ynov.com>
2019-09-12 16:09:19 +0200

    gamePage fixed

--- src/App.js
+++ src/App.js
@@ -11,111 +11,6 @@ import { resolve } from 'path';
 
 
 class App extends Component {
-  state = {
-    combatInfo: 'Waiting...',
-    playerTest: new Player('Clement'),
-    monsterTest: new Monster('Florent'),
-    playerHP: null,
-    monsterHP: null,
-    counter: 0,
-    gold: 0
-  }
-
-  // testCombat = () => {
-  //   let counter = 0
-  //   const TM = new Player('TM')
-  //   this.setState({combatInfo: 'The battle begin'})
-
-  //   while (TM.stats.Alive) {
-  //       const florentMonster = new Monster('florent')
-
-  //       while (florentMonster.stats.Alive && TM.stats.Alive) {
-  //           this.setState({combatInfo: 'Player is attacking !'})
-  //           TM.Attack(florentMonster)
-  //           if (florentMonster.stats.Alive) {
-  //               this.setState({combatInfo: 'The monster fight back !'})
-  //               florentMonster.Attack(TM)
-  //               this.setState({combatInfo: TM.stats.Username + ' got ' + TM.stats.Life + ' HP left.'})
-  //           }
-  //       }
-  //       counter +=1             
-  //   }
-  //   this.setState({combatInfo: 'Vous êtes mort... Vous avez tué ' + counter + ' Florent'})
-  // }
-
-  //DEBUT TEST 1
-
-  createCombat = (player, monster, callback) => {
-    this.setState({playerHP: player.stats.Life})
-    this.setState({monsterHP: monster.stats.Life})
-
-    if (player.stats.Alive) {
-      if (monster.stats.Alive) {
-        setTimeout(() => callback('The battle begin'), 500)
-      } else {
-        setTimeout(() => callback('...but the farming is never ending !'), 500)
-      }     
-    } else {
-      const goldLost = player.stats.Gold / 10
-      player.stats.Gold -= goldLost
-      this.setState({gold: player.stats.Gold,})
-      this.setState({combatInfo: 'You are dead. Heal yourself before going back. You killed ' + this.state.counter + ' monster. You lost ' + goldLost + ' gold.'})
-    }   
-  }
-
-  playerAlive = (player, monster, callback) => {
-    setTimeout(() => callback(player.stats.Username + ' is going to attack !'), 500)   
-  }
-
-  bothAlive = (player, monster, callback) => {
-    if (monster.stats.Alive) {
-      setTimeout(() => callback(player.Attack(monster)), 500)  
-    }
-    else {
-      monster.stats.Life = 80
-      this.setState({monsterHP: monster.stats.Life, combatInfo: 'Another monster is coming !'})
-      setTimeout(() => callback(player.Attack(monster)), 500)  
-    }
-     
-  }
-
-  monsterAlive = (player, monster, callback) => {
-    this.setState({monsterHP: monster.stats.Life})
-
-    if (monster.stats.Alive) {
-      setTimeout(() => callback(monster.Attack(player)), 500)
-    } else {
-      const goldEarned = monster.randomInt(100)
-      player.stats.Gold += goldEarned
-      this.setState({counter: this.state.counter + 1, gold: player.stats.Gold,})
-      setTimeout(() => callback('You killed a monster. You earned ' + goldEarned + ' gold'), 500)
-    }
-    
-    
-  }
-
-  testCombat2 = (player, monster, callback) => {
-    
-    this.createCombat(player, monster, message => {
-      this.setState({combatInfo: message})
-      
-      this.playerAlive(player, monster, message2 => {
-        this.setState({combatInfo: message2})
-
-        this.bothAlive(player, monster, message3 => {
-          this.setState({combatInfo: message3})
-
-          this.monsterAlive(player, monster, message4 => {
-            this.setState({combatInfo: message4})
-
-            this.testCombat2(player, monster)
-          })
-        })
-      })
-    })
-  }
-
-  // FIN TEST 1
 
   // DEBUT TEST 2
 
@@ -152,22 +47,13 @@ class App extends Component {
 
   // FIN TEST 2
 
-
-
-
-  
-
-
-
   render() {
     return ( 
       <div className="main-div">
           <Router>
             {/* Routing */}
             <Route exact path="/" component={MainPage} />
-            <Route path="/game" render={(props) => <GamePage {...props} displayPlayerHP={this.state.playerHP} displayMonsterHP={this.state.monsterHP} displayGold={this.state.gold}displayCombat={this.state.combatInfo} startCombat={() => this.testCombat2(this.state.playerTest, this.state.monsterTest, messageInfo => {
-              this.setState({combatInfo: messageInfo}) 
-            })} />}/>
+            <Route path="/game" render={(props) => <GamePage {...props} />}/>
             <Route path="/test" component={testCombat} />
         </Router>
       </div>
--- src/components/GamePage.js
+++ src/components/GamePage.js
@@ -6,15 +6,25 @@ import CharacterStuff from "./gameplay/CharacterStuff/CharacterStuff.js";
 import Wrought from "./gameplay/Wrought";
 import Shop from "./gameplay/Shop/Shop";
 import Room from "./gameplay/Room/Room.js"
+import Player from "./characters/Player"
+import Monster from "./characters/Monster"
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 class GamePage extends Component {
     constructor() {
         super()
-        this.state = { gameplayElement: 'inventary' }
+        this.state = { 
+            gameplayElement: 'inventary',
+            combatInfo: 'Waiting...',
+            playerTest: new Player('Clement'),
+            monsterTest: new Monster('Florent'),
+            playerHP: null,
+            monsterHP: null,
+            counter: 0,
+            gold: 0
+        }
     }
-
-
+    
     toggleElements() {
         switch (this.state.gameplayElement) {
             case 'inventary' :
@@ -50,13 +60,85 @@ class GamePage extends Component {
         }
     }
 
+    createCombat = (player, monster, callback) => {
+        this.setState({playerHP: player.stats.Life})
+        this.setState({monsterHP: monster.stats.Life})
+    
+        if (player.stats.Alive) {
+          if (monster.stats.Alive) {
+            setTimeout(() => callback('The battle begin'), 500)
+          } else {
+            setTimeout(() => callback('...but the farming is never ending !'), 500)
+          }     
+        } else {
+          const goldLost = player.stats.Gold / 10
+          player.stats.Gold -= goldLost
+          this.setState({gold: player.stats.Gold,})
+          this.setState({combatInfo: 'You are dead. Heal yourself before going back. You killed ' + this.state.counter + ' monster. You lost ' + goldLost + ' gold.'})
+        }   
+      }
+    
+      playerAlive = (player, monster, callback) => {
+        setTimeout(() => callback(player.stats.Username + ' is going to attack !'), 500)   
+      }
+    
+      bothAlive = (player, monster, callback) => {
+        if (monster.stats.Alive) {
+          setTimeout(() => callback(player.Attack(monster)), 500)  
+        }
+        else {
+          monster.stats.Life = 80
+          this.setState({monsterHP: monster.stats.Life, combatInfo: 'Another monster is coming !'})
+          setTimeout(() => callback(player.Attack(monster)), 500)  
+        }
+         
+      }
+    
+      monsterAlive = (player, monster, callback) => {
+        this.setState({monsterHP: monster.stats.Life})
+    
+        if (monster.stats.Alive) {
+          setTimeout(() => callback(monster.Attack(player)), 500)
+        } else {
+          const goldEarned = monster.randomInt(100)
+          player.stats.Gold += goldEarned
+          this.setState({counter: this.state.counter + 1, gold: player.stats.Gold,})
+          setTimeout(() => callback('You killed a monster. You earned ' + goldEarned + ' gold'), 500)
+        }
+        
+        
+      }
+    
+      testCombat2 = (player, monster, callback) => {
+        
+        this.createCombat(player, monster, message => {
+          this.setState({combatInfo: message})
+          
+          this.playerAlive(player, monster, message2 => {
+            this.setState({combatInfo: message2})
+    
+            this.bothAlive(player, monster, message3 => {
+              this.setState({combatInfo: message3})
+    
+              this.monsterAlive(player, monster, message4 => {
+                this.setState({combatInfo: message4})
+    
+                this.testCombat2(player, monster)
+              })
+            })
+          })
+        })
+      }
+
     render() {
         return (
             <div>
                 <div className="d-flex justify-content-around">
                     <h1 className="my-3 text-white text-center">Farming in a Nutshell</h1>
                     
-                    <Button onClick={this.props.startCombat}>Begin</Button>
+                    <Button onClick={() => this.testCombat2(this.state.playerTest, this.state.monsterTest, messageInfo => {
+                    this.setState({combatInfo: messageInfo}) 
+                    })}>Begin</Button>
                     <a className="btn btn-logout btn-warning mt-3" href="/">Logout</a>
                 </div>
 
@@ -67,19 +149,19 @@ class GamePage extends Component {
                             <img width="700" src="/img/gamescene_1.png" />
                             {/*<img src="https://aliceasmartialarts.com/wp-content/uploads/2017/04/default-image.jpg"/>*/}
                             <div class="gameplay-infos border py-3 px-3">
-                                <a>{this.props.displayCombat}</a> 
+                                <a>{this.state.combatInfo}</a> 
                             </div>
                             <br/>
                             <div class="gameplay-infos border py-3 px-3">
-                                <a>Player HP : {this.props.displayPlayerHP}</a> 
+                                <a>Player HP : {this.state.playerHP}</a> 
                             </div>
                             <br/>
                             <div class="gameplay-infos border py-3 px-3">
-                                <a>Monster HP : {this.props.displayMonsterHP}</a> 
+                                <a>Monster HP : {this.state.monsterHP}</a> 
                             </div>
                             <br/>
                             <div class="gameplay-infos border py-3 px-3">
-                                <a>Gold : {this.props.displayGold}</a> 
+                                <a>Gold : {this.state.gold}</a> 
                             </div>
                         </div>
                         <div name='conteneut_interface' className="col-6">
