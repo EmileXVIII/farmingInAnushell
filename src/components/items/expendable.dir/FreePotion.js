b321e95e5f4d5e21b9ccee8adfe0700a881b4121
emile roche ubuntu <emile.roche@ynov.com>
2019-09-24 17:34:24 +0200

    improve system FreePotion Ok

--- src/components/items/expendable.dir/FreePotion.js
+++ src/components/items/expendable.dir/FreePotion.js
@@ -17,8 +17,8 @@ class FreePotion {
         this.len = this.possibilities.length;
         this.status = undefined;
         this.process = this.process.bind(this);
-        this.setTime.call(this, this.time);
-        this.setTime.call(this, this.timeAfterPreviousInput);
+        setTime.call(this, 'time');
+        setTime.call(this, 'timeAfterPreviousInput');
     }
     async process() {
         while (true) {
@@ -32,18 +32,12 @@ class FreePotion {
                 this.status = false;
                 console.log('error Catched')
             }
-            document.removeEventListener('keypress', (event) => {
-                if (this.possibilities.indexOf(event.code) !== -1) {
-                    this.timeAfterPreviousInput = 0;
-                    if (this.check(event.code)) return true; else throw (new Error('wrongTouch'))
-                }
-            });
             if (this.status === true) {
-                this.value += 20 - Math.trunc(this.timeAfterPreviousInput / 100);
+                this.value += (20 - Math.trunc(this.timeAfterPreviousInput / 100))/5;
                 if (this.value < 0) { this.value = 0 };
             }
-            else if (this.time<5000){
-            this.value -= 20;
+            else if (this.time < 5000) {
+                this.value -= 20;
                 if (this.value < 0) { this.value = 0 };
             }
             if (this.time > 5000) {
@@ -55,33 +49,46 @@ class FreePotion {
                 this.time = 0;
                 this.timeAfterPreviousInput = 0;
                 this.value = 0;
-                this.answer = promesseFreePotion.call(this);
             }
             let ind = Math.trunc(Math.random() * this.len)
             this.img = this.imgsUrl[ind];
+            this.keyToPress=this.possibilities[ind];
             gestionnaireEvents.emit('newFreePotionImg', this.img);
+            this.answer = promesseFreePotion.call(this);
 
         }
 
     }
     check = (keyPress) => { return keyPress === this.keyToPress ? true : false }
-    setTime = (time) => {
+}
+    function setTime(time) {
+        console.log('InSetTime')
         setTimeout(() => {
-            time += 10;
-            this.setTime(time)
+            this[time] += 10;
+            setTime.call(this,time)
         }
             , 10)
     }
-}
 
-async function promesseFreePotion() {
-    document.addEventListener('keypress', (event) => {
-        if (this.possibilities.indexOf(event.code) !== -1) {
-            this.timeAfterPreviousInput = 0;
-            if (this.check(event.code)) return true; else throw (new Error('wrongTouch'))
-        }
+
+function promesseFreePotion() {
+    return new Promise((resolve, reject)=>{
+        console.log(this);
+        window.removeEventListener('keydown', (event) => {
+            if (this.possibilities.indexOf(event.code) !== -1) {
+                this.timeAfterPreviousInput = 0;
+                if (this.check(event.code)) resolve(true); else reject('wrongTouch')
+            }
+        });
+        window.addEventListener('keydown', (event) => {
+            console.log('FreePotionKeyPress',event.key)
+            if (this.possibilities.indexOf(event.key) !== -1) {
+                this.timeAfterPreviousInput = 0;
+                if (this.check(event.code)) resolve(true); else reject('wrongTouch')
+            }
+        })
+        setTimeout(() => {console.log('In timeout'); reject('TimeOut') }, 5000);
     })
-    setTimeout(() => {if (this.time>5000) throw (new Error('TimeOut')) }, 5000)
 }
 
 export default FreePotion;
