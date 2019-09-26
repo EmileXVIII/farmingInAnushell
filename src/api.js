81b7aa639585164a6e48a568a1b0e6e89fca4041
Colin TENAGUILLO <colin.tenaguillo@ynov.com>
2019-09-26 16:01:50 +0200

    Enable CORS

--- src/api.js
+++ src/api.js
@@ -1,37 +1,34 @@
 const mysql = require('mysql')
 const app = require('express')()
+const cors = require('cors');
 const PORT = process.env.PORT || 8080
 let dbConn = ""
 
 // Connection bdd local
-// app.use(function (req, res, next) {
-//     dbConn = mysql.createConnection({
-//         host: 'localhost',
-//         user: 'debian-sys-maint',
-//         password: 'phrHtsSP5Hoq6EYl',
-//         database: 'farmingInAnutshell'
-//     });
-//     dbConn.connect();
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// });
-
-// Connection bdd Serveur FarmingNutshell
 app.use(function (req, res, next) {
     dbConn = mysql.createConnection({
         host: 'localhost',
-        user: 'padmin',
-        password: 'D3b14nr0oT',
-        database: 'FarmingInAnutshell'
+        user: 'debian-sys-maint',
+        password: 'phrHtsSP5Hoq6EYl',
+        database: 'farmingInAnutshell'
     });
     dbConn.connect();
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+
+// Connection bdd Serveur FarmingNutshell
+// app.use(function (req, res, next) {
+//     dbConn = mysql.createConnection({
+//         host: 'localhost',
+//         user: 'padmin',
+//         password: 'D3b14nr0oT',
+//         database: 'FarmingInAnutshell'
+//     });
+//     dbConn.connect();
+//     next();
+// });
+
+app.use(cors())
 
 app.get('/user/:email/pwd', (req, res) => {
     const email = req.params.email
