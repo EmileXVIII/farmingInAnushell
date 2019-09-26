8fe23719448162a5849354dbeb8f5a4a5ac13654
Colin TENAGUILLO <colin.tenaguillo@ynov.com>
2019-09-26 15:46:59 +0200

    Login operationnel

--- src/api.js
+++ src/api.js
@@ -4,27 +4,12 @@ const PORT = process.env.PORT || 8080
 let dbConn = ""
 
 // Connection bdd local
-app.use(function (req, res, next) {
-    dbConn = mysql.createConnection({
-        host: 'localhost',
-        user: 'debian-sys-maint',
-        password: 'phrHtsSP5Hoq6EYl',
-        database: 'farmingInAnutshell'
-    });
-    dbConn.connect();
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
-// Connection bdd Serveur FarmingNutshell
 // app.use(function (req, res, next) {
 //     dbConn = mysql.createConnection({
 //         host: 'localhost',
-//         user: 'padmin',
-//         password: 'D3b14nr0oT',
-//         database: 'FarmingInAnutshell'
+//         user: 'debian-sys-maint',
+//         password: 'phrHtsSP5Hoq6EYl',
+//         database: 'farmingInAnutshell'
 //     });
 //     dbConn.connect();
 //     res.header("Access-Control-Allow-Origin", "*");
@@ -32,6 +17,21 @@ app.use(function (req, res, next) {
 //     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 //     next();
 // });
+
+// Connection bdd Serveur FarmingNutshell
+app.use(function (req, res, next) {
+    dbConn = mysql.createConnection({
+        host: 'localhost',
+        user: 'padmin',
+        password: 'D3b14nr0oT',
+        database: 'FarmingInAnutshell'
+    });
+    dbConn.connect();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.get('/user/:email/pwd', (req, res) => {
     const email = req.params.email
